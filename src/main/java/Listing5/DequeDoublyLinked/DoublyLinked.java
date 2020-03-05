ff09543048df4a07107aea0eba3a65a74eb5e1d5
mj <madjo85@yandex.ru>
2020-03-05 17:03:53 +0300

    Listing5 change DoublyLinked

--- src/main/java/Listing5/DequeDoublyLinked/DoublyLinked.java
+++ src/main/java/Listing5/DequeDoublyLinked/DoublyLinked.java
@@ -19,9 +19,79 @@ public class DoublyLinked {
             last = temLink;
         } else {
             first.setPrevious(temLink);
-            temLink.setNext(first);
         }
+        temLink.setNext(first);
         first = temLink;
     }
 
+    public void insertLast(long dd) {
+        Link tempLink = new Link(dd);
+        if (isEmpty()) {
+            first = tempLink;
+        } else {
+            last.setNext(tempLink);
+            tempLink.setPrevious(last);
+        }
+        last = tempLink;
+    }
+
+    public Link deleteFirst() {
+       Link tempLink = first;
+       if (first.getNext() == null) {
+           last = null;
+       } else {
+           first.getNext().setPrevious(null);
+       }
+       first = first.getNext();
+       return tempLink;
+    }
+
+    public Link deleteLast() {
+        Link tempLink = last;
+        if (first.getNext() == null) {
+            first = null;
+        } else
+            last.getPrevious().setNext(null);
+        last = last.getPrevious();
+        return tempLink;
+    }
+
+    public void insertAfter(long dPos, long dd) {
+        if (isEmpty()) {
+            first = new Link(dd);
+            last = first;
+            return;
+        }
+        Link current = first;
+        while (current.getNext() != null) {
+            if (dPos > current.getdData()) {
+               break;
+            } else {
+                current = current.getNext();
+            }
+        }
+        Link tempLink = new Link(dd);
+        if (current == first) {
+            first = tempLink;
+            tempLink.setNext(current);
+            current.setPrevious(tempLink);
+            return;
+        } else if (current == last) {
+            last = tempLink;
+        } else {
+            tempLink.setNext(current.getNext());
+            current.getNext().setPrevious(tempLink);
+        }
+        current.setNext(tempLink);
+        tempLink.setPrevious(current);
+    }
+    public void display() {
+        Link current = first;
+        while (current.getNext() != null) {
+            current.displayLink();
+            current = current.getNext();
+        }
+        current.displayLink();
+        last.displayLink();
+    }
 }
--- src/test/java/Listing5/TestDequeDoublyLinked.java
+++ src/test/java/Listing5/TestDequeDoublyLinked.java
@@ -7,5 +7,12 @@ public class TestDequeDoublyLinked {
     @Test
     public void testDoublyLinkedList() {
         DoublyLinked doublyLinked = new DoublyLinked();
+        doublyLinked.insertFirst(21);
+        doublyLinked.insertFirst(33);
+        doublyLinked.insertFirst(44);
+        doublyLinked.insertFirst(55);
+        doublyLinked.insertFirst(66);
+        doublyLinked.insertAfter(22, 49);
+        doublyLinked.display();
     }
 }
