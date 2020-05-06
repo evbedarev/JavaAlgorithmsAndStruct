733c027ac55a499e8c779426859e1cf3a218df1b
mj <madjo85@yandex.ru>
2020-05-06 15:15:35 +0300

    task55

---
+++ src/main/java/Listing5/task_5_5/FlaviyTask.java
@@ -0,0 +1,18 @@
+package Listing5.task_5_5;
+
+import Listing5.stack_cyclical_list.StackOnCyclicalList;
+
+public class FlaviyTask {
+    private int begins;
+    private int numPositions;
+    private StackOnCyclicalList stack = new StackOnCyclicalList();
+
+    public FlaviyTask(int begins, int numPositions) {
+        this.begins = begins;
+        this.numPositions = numPositions;
+    }
+
+    public void calculate() {
+    }
+
+}
